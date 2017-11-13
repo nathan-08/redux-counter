831cf9f4d206ad4bea1ab12af7b22d08bcf434e8
Nathan Klundt <nathanryan001@gmail.com>
2017-11-13 14:51:48 -0700

    completed all tasks

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
+import { increment, decrement, undo, redo } from './ducks/counter'
 
 import "./App.css";
 
 export class App extends Component {
 	render() {
+		console.log('this.props: ', this.props)
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{this.props.currentValue}</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={() => this.props.increment(1)}
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={() => this.props.increment(5)}
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={() => this.props.decrement(1)}
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={() => this.props.decrement(5)}
 						>
 							-5
 						</button>
 						<br />
 						<button
 							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={this.props.previousValues.length === 0}
+							onClick={() => this.props.undo()}
 						>
 							Undo
 						</button>
 						<button
 							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={this.props.futureValues.length === 0}
+							onClick={() => this.props.redo()}
 						>
 							Redo
 						</button>
@@ -52,12 +55,15 @@ export class App extends Component {
 				</section>
 				<section className="state">
 					<pre>
-						{ JSON.stringify( this.props, null, 2 ) }
+						{JSON.stringify(this.props, null, 2)}
 					</pre>
 				</section>
 			</div>
 		);
 	}
 }
-
-export default App;
+function mapStateToProps(state) {
+	return state;
+}
+const decorator = connect(mapStateToProps, { increment, decrement, undo, redo })
+export default decorator(App);
--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,58 @@
 // change this to true to see test results on the black diamond section.
-export const BLACK_DIAMOND = false;
+export const BLACK_DIAMOND = true;
+
+const initialState = {
+    currentValue: 0,
+    futureValues: [],
+    previousValues: []
+}
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const UNDO = 'UNDO';
+const REDO = 'REDO';
+//action creators
+export function increment(amount) {
+    return {
+        type: INCREMENT,
+        amount: amount
+    }
+}
+export function decrement(amount) {
+    return {
+        type: DECREMENT,
+        amount: amount
+    }
+}
+export function undo(){
+    return {
+        type: UNDO
+    }
+}
+export function redo(){
+    return {
+        type: REDO
+    }
+}
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            return Object.assign({}, state, { currentValue: state.currentValue + action.amount,
+                                              futureValues: [],
+                                              previousValues: [state.currentValue, ...state.previousValues] })
+        case DECREMENT:
+            return Object.assign({}, state, { currentValue: state.currentValue + action.amount,
+                                              futureValues: [],
+                                              previousValues: [state.currentValue, ...state.previousValues] })
+        case UNDO: 
+            return Object.assign({}, state, { currentValue: state.previousValues[0], 
+                                              futureValues: [state.currentValue, ...state.futureValues],
+                                              previousValues: state.previousValues.slice(1)})
+        case REDO:
+            return Object.assign({}, state, { currentValue: state.futureValues[0],
+                                              futureValues: state.futureValues.slice(1),
+                                              previousValues: [state.currentValue, ...state.previousValues]})
+
+        default:
+            return state;
+    }
+}
